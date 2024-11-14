c46d84d403945d88f22aa8208f15f3ef2bcf0873
Sooty001 <goodg0311@gmail.com>
2024-11-15 02:06:02 +0300

    add main4

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
-        System.out.print("+ Выполнено");
+        System.out.print("+ Выполнено задание 2");
     }
 }
