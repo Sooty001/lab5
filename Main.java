1d0f720b412839205b8f4aca9fc98ea780856483
Sooty001 <goodg0311@gmail.com>
2024-11-15 02:11:16 +0300

    update main

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("+ Выполнено задание 1");
+        System.out.print("+ Изменения");
     }
 }
